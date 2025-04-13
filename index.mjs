fb7ac019f0943a53d8c8fe60b05464a639f677a9
ahadsts9901 <ahadsts990@gmail.com>
2025-04-13 17:31:13 +0500

    code

--- index.mjs
+++ index.mjs
@@ -6,4 +6,4 @@ app.use("/", (req, res) => res.send("hello from elasticbean stalk"))
 
 const PORT = process.env.PORT || 5002
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`))
+app.listen(PORT, "0.0.0.0",() => console.log(`server running on port ${PORT}`))
