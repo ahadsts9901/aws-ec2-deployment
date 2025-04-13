435eb29ebe6c9929901126e2764fe909a47473b5
ahadsts9901 <ahadsts990@gmail.com>
2025-04-13 19:47:15 +0500

    c

--- index.mjs
+++ index.mjs
@@ -2,8 +2,8 @@ import express from "express"
 
 const app = express()
 
-app.use("/", (req, res) => res.send("hello from ec2"))
+app.use("/", (req, res) => { res.send("hello from ec2") })
 
 const PORT = process.env.PORT || 5003
 
-app.listen(PORT, "0.0.0.0",() => console.log(`server running on port ${PORT}`))
+app.listen(PORT, "0.0.0.0", () => console.log(`server running on port ${PORT}`))
